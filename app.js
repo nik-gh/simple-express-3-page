a72dc516f0912e6dfea31f519986b13f235bc102
Nik <32958339+nik-gh@users.noreply.github.com>
2019-04-10 11:28:19 +0200

    removing leftovers

--- app.js
+++ app.js
@@ -8,14 +8,7 @@ const bodyParser = require('body-parser');
 const routes = require('./routes/index');
 const about = require('./routes/about');
 const contact = require('./routes/contact');
-// "body-parser": "~1.15.1",
-// "cookie-parser": "~1.4.3",
-// "debug": "~2.2.0",
-// "express": "~4.13.4",
-// "jade": "~1.11.0",
-// "morgan": "~1.7.0",
-// "serve-favicon": "~2.3.0",
-// "nodemailer": "*"
+
 const app = express();
 
 // view engine setup
